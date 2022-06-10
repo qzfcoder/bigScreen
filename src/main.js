03d2bd9ffa82cedee8f814ef8aeb0d0c5e56d3f7
qzfcoder <867303253@qq.com>
2022-06-10 13:42:20 +0800

    x

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+// 引入echarts
+import * as echarts from "echarts"; //引入echarts
+Vue.prototype.$echarts = echarts; //注册组件
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+  render: function (h) {
+    return h(App);
+  },
+}).$mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import bigScreen from '../views/bigScreen.vue'
 
 Vue.use(VueRouter)
 
@@ -11,15 +12,11 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-  }
+    path: '/bigScreen',
+    name: 'bigScreen',
+    component: bigScreen
+  },
+
 ]
 
 const router = new VueRouter({
